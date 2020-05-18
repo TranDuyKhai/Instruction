74e6a7e877085e7a5324e7817959da71a8415fcd
lanhsotret <58644190+lanhsotret@users.noreply.github.com>
2020-05-18 14:02:54 +0700

    complete layout
    anhlanh

---
+++ js/general.js
@@ -0,0 +1,9 @@
+$('.workStyle__figure').slick({
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    fade: true,
+    speed: 3000,
+    slidesToScroll: 1,
+    slidesToShow: 1
+})

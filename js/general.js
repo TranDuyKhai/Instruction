fb0d4f32407a9a997fcd80061600769a1c0b048e
lanhsotret <58644190+lanhsotret@users.noreply.github.com>
2020-05-22 09:36:53 +0700

    fix on ie11
    anhlanh

--- js/general.js
+++ js/general.js
@@ -28,11 +28,11 @@ $(function() {
         duration: 500,
         anchorPlacement: 'center-center'
     });
-    $('#svgPlay').html(`<svg 
-    xmlns="http://www.w3.org/2000/svg"
-    xmlns:xlink="http://www.w3.org/1999/xlink"
-    width="81px" height="81px"  viewBox="0 0 81 81">
-   <path fill-rule="evenodd"  opacity="0.8" fill="rgb(255, 255, 255)"
-    d="M40.500,80.500 C18.408,80.500 0.499,62.591 0.499,40.499 C0.499,18.407 18.408,0.498 40.500,0.498 C62.592,0.498 80.501,18.407 80.501,40.499 C80.501,62.591 62.592,80.500 40.500,80.500 ZM33.000,25.999 L33.000,55.999 L50.000,40.999 L33.000,25.999 Z"/>
-   </svg>`);
+    $('#svgPlay').html('<svg ' +
+    'xmlns="http://www.w3.org/2000/svg"' +
+    'xmlns:xlink="http://www.w3.org/1999/xlink"' +
+    'width="81px" height="81px"  viewBox="0 0 81 81">' +
+   '<path fill-rule="evenodd"  opacity="0.8" fill="rgb(255, 255, 255)"' +
+    'd="M40.500,80.500 C18.408,80.500 0.499,62.591 0.499,40.499 C0.499,18.407 18.408,0.498 40.500,0.498 C62.592,0.498 80.501,18.407 80.501,40.499 C80.501,62.591 62.592,80.500 40.500,80.500 ZM33.000,25.999 L33.000,55.999 L50.000,40.999 L33.000,25.999 Z"/>' +
+   '</svg>');
 });
